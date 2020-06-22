f49adda93eb49708536880e6e0b9180736a67f01
kiwijang <kiwijang5473@gmail.com>
2020-06-22 22:12:36 +0800

    1. 修改 github.sh 和 num.sh 的輸入參數的方式 / 2. 修改 github.sh 的 curl 後 -s(--silent) 不顯示，也不產生檔案並存成 (> curlResult.txt)

--- homeworks/week1/github.sh
+++ homeworks/week1/github.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-read -p "請輸入 github 帳號: " user
-touch curlResult.txt
-url='https://api.github.com/users/'${user}
-curl $url > curlResult.txt
+# 使用方式為(請在 bash 中輸入):
+# 
+# ./github.sh <要搜尋的帳號>
+# 或
+# sh github.sh <要搜尋的帳號>
+# 
+# 如: sh github.sh aszx87410
+
+url='https://api.github.com/users/'$1
+
+echo '搜尋網址:' $url
 echo "取得結果:"
-echo " "
+echo -e "=======================================================\n"
 echo "使用者暱稱:"
-sed -E 's/\},\s*\{/\},\n\{/g' curlResult.txt | grep '"name"' | cut -d '"' -f 4  
-echo " "
+curl -s $url | sed -E 's/\},\s*\{/\},\n\{/g' | grep '"name"' | cut -d '"' -f 4  
+echo -e "\n"
 echo "介紹:"
-sed -E 's/\},\s*\{/\},\n\{/g' curlResult.txt | grep '"bio"' | cut -d '"' -f 4
-echo " "
+curl -s $url | sed -E 's/\},\s*\{/\},\n\{/g' | grep '"bio"' | cut -d '"' -f 4
+echo -e "\n"
 echo "地點:"
-sed -E 's/\},\s*\{/\},\n\{/g' curlResult.txt | grep '"location"' | cut -d '"' -f 4
-echo " "
+curl -s $url | sed -E 's/\},\s*\{/\},\n\{/g' | grep '"location"' | cut -d '"' -f 4
+echo -e "\n"
 echo "個人網站:"
-sed -E 's/\},\s*\{/\},\n\{/g' curlResult.txt | grep '"blog"' | cut -d '"' -f 4
+curl -s $url | sed -E 's/\},\s*\{/\},\n\{/g' | grep '"blog"' | cut -d '"' -f 4
--- homeworks/week1/num.sh
+++ homeworks/week1/num.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# $1 為傳進來的參數，$0 為檔名
+# 資料來源:
+# https://www.runoob.com/linux/linux-shell-passing-arguments.html?fbclid=IwAR34LxgZswLmHMnMWK4KWdn21gdNuDLH763zxQnmRu-j7pzzOlAbxA6eyOw
+#
+# 使用方式為(請在 bash 中輸入):
+# ./num.sh <要產生幾個 js 檔案>
+# 或
+# sh num.sh <要產生幾個 js 檔案>
+# 
+# 如: sh num.sh 5
 
-read -p "輸入你要產生幾個 js(請輸入正整數): " num
 i=1
-while [ $i != $(($num+1)) ]
+while [ $i != $(($1+1)) ]
 do
 	touch "$i.js"
 	i=$(($i+1))	
