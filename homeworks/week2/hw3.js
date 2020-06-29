b38b7efa09d80a06cff2959de95c113b7d8d80e3
naomi <kiwijang5473@gmail.com>
2020-06-30 00:10:18 +0800

    week2 upd

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+    const arr = [];
+    const strArr = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        arr.push(strArr.pop());
+    }
+    console.log(arr.join(''));
 }
 
 reverse('hello');
