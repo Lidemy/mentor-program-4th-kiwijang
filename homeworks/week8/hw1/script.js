30d46ef1004a36b86c03c9e90d898d6b97f1b7f0
naomi <kiwijang5473@gmail.com>
2020-08-23 08:29:37 +0800

    upd hw1 修改style成用class、btn 倒回前一頁

--- homeworks/week8/hw1/script.js
+++ homeworks/week8/hw1/script.js
@@ -4,9 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.box__btn').addEventListener('click', () => {
     getData();
   });
-  document.querySelector('.prizebox__btn').addEventListener('click', () => {
-    getData();
-  });
 
   function getData() {
     const xhr = new XMLHttpRequest();
@@ -14,11 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
       if (xhr.status >= 200 && xhr.status < 400) {
         try {
           const data = JSON.parse(xhr.response);
-          if (data.prize) {
-            changePageInfo(data.prize);
-          } else {
+          if (!data.prize) {
             window.alert('系統不穩定，請再試一次');
+            return;
           }
+
+          changePageInfo(data.prize);
         } catch (err) {
           window.alert(`發生錯誤，請重新整理頁面再試一遍，或與程式管理員聯絡，謝謝您。\n${err}`);
         }
@@ -34,35 +32,36 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function changePageInfo(prizeType) {
+    document.querySelector('.box').classList.add('d-none');
+    document.querySelector('.prizebox').classList.remove('d-none');
+    if (document.querySelector('.section-main').classList.contains('first-priz')) {
+      document.querySelector('.section-main').classList.remove('first-priz');
+    }
+    if (document.querySelector('.section-main').classList.contains('second-priz')) {
+      document.querySelector('.section-main').classList.remove('second-priz');
+    }
+    if (document.querySelector('.section-main').classList.contains('third-priz')) {
+      document.querySelector('.section-main').classList.remove('third-priz');
+    }
+    if (document.querySelector('.section-main').classList.contains('none-priz')) {
+      document.querySelector('.section-main').classList.remove('none-priz');
+    }
     switch (prizeType) {
       case 'FIRST':
-        document.querySelector('.box').classList.add('d-none');
-        document.querySelector('.prizebox').classList.remove('d-none');
         document.querySelector('.prizebox > h2').innerText = '恭喜你中頭獎了！日本東京來回雙人遊！';
-        document.querySelector('.section-main').style.color = 'black';
-        document.querySelector('.section-main').style.backgroundImage = 'url("./img/flight.jpg")';
+        document.querySelector('.section-main').classList.add('first-priz');
         break;
       case 'SECOND':
-        document.querySelector('.box').classList.add('d-none');
-        document.querySelector('.prizebox').classList.remove('d-none');
         document.querySelector('.prizebox > h2').innerText = '二獎！90 吋電視一台！';
-        document.querySelector('.section-main').style.color = 'black';
-        document.querySelector('.section-main').style.backgroundImage = 'url("./img/tv.jpg")';
+        document.querySelector('.section-main').classList.add('second-priz');
         break;
       case 'THIRD':
-        document.querySelector('.box').classList.add('d-none');
-        document.querySelector('.prizebox').classList.remove('d-none');
         document.querySelector('.prizebox > h2').innerText = '恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！';
-        document.querySelector('.section-main').style.color = 'black';
-        document.querySelector('.section-main').style.backgroundImage = 'url("./img/yt.jpg")';
+        document.querySelector('.section-main').classList.add('third-priz');
         break;
       case 'NONE':
-        document.querySelector('.box').classList.add('d-none');
-        document.querySelector('.prizebox').classList.remove('d-none');
         document.querySelector('.prizebox > h2').innerText = '銘謝惠顧';
-        document.querySelector('.section-main').style.color = 'white';
-        document.querySelector('.section-main').style.backgroundImage = 'none';
-        document.querySelector('.section-main').style.backgroundColor = 'black';
+        document.querySelector('.section-main').classList.add('none-priz');
         break;
       default:
         break;
